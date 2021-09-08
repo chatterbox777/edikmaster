dc16f6b363dc1176a4759ab9d0fa484f0c16692d
chatterbox777 <chatterbox1995@mail.ru>
2021-09-08 11:50:25 +0300

    make helper function to format price in ticket, make some css and ui logic for Ticket.tsx

--- src/components/Aviasales/SubComponents/Ticket/Ticket.tsx
+++ src/components/Aviasales/SubComponents/Ticket/Ticket.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { formatPrice } from '../../../../helpers/formatAviaPrice';
+
 const Ticket = ({ tickets, isLoading }) => {
   if (isLoading) {
     return null;
@@ -7,7 +9,14 @@ const Ticket = ({ tickets, isLoading }) => {
     return (
       <>
         {tickets?.length > 0 && !isLoading ? (
-          tickets.map(ticket => <DivTicket key={ticket.carrier + ticket.price}>{ticket.carrier}</DivTicket>)
+          tickets.map(ticket => (
+            <DivTicket key={ticket.carrier + ticket.price}>
+              <DivPriceAndLabel>
+                <Price>{formatPrice(ticket.price)}</Price>
+                <div>S7</div>
+              </DivPriceAndLabel>
+            </DivTicket>
+          ))
         ) : (
           <div>Empty</div>
         )}
@@ -18,6 +27,21 @@ const Ticket = ({ tickets, isLoading }) => {
 
 export default Ticket;
 
+const DivPriceAndLabel = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 20px;
+`;
+const Price = styled.h3`
+  color: #2196f3;
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 24px;
+  margin: 0;
+`;
 const DivTicket = styled.div`
   width: 502px;
   height: 184px;
---
+++ src/helpers/formatAviaPrice.ts
@@ -0,0 +1,16 @@
+export const formatPrice = price => {
+  let result;
+  let newArr = price.toString().split('');
+  switch (newArr.length) {
+    case 5:
+      newArr.splice(2, 0, ' ');
+      result = newArr.join('') + ' ' + 'Р';
+      return result;
+    case 6:
+      newArr.splice(3, 0, ' ');
+      result = newArr.join('') + ' ' + 'Р';
+      return result;
+    default:
+      break;
+  }
+};
